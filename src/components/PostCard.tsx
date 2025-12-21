5d75b651bb43f3ed9751c0d21486e47bfd7164c5
Adem Ademi <ademiadem70@gmail.com>
2025-12-21 23:24:14 +0100

    Last changes

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -12,16 +12,18 @@ export default function PostCard({ post }: PostCardProps) {
         <img
           src={image}
           alt={post.title.rendered}
-          className="w-full h-48 object-cover"
+          className="w-full h-60 object-cover"
         />
       )}
 
+    <a href={`/post/${post.slug}`} className="block">
       <div className="p-4">
         <h2
           className="font-bold text-lg"
           dangerouslySetInnerHTML={{ __html: post.title.rendered }}
         />
       </div>
+      </a>
     </article>
   );
 }
